6c4a61844addd1347fe8f4580b1aeed686ae8d94
Rishav <rishavkamboj75@gmail.com>
2024-11-04 22:49:53 +0530

    Today commit

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -30,17 +30,19 @@ const Navbar = () => {
           </ul>
           {!user ? (
             <div className="flex items-center gap-3">
-              {/* Login Button */}
-              <Button
-                className="border  border-gray-300 text-gray-700 px-4 py-2 rounded-3xl hover:border-gray-500"
-                variant="outline"
-              >
-                Login
-              </Button>
-              {/* Signup Button */}
-              <Button className="bg-[#6A38C2] text-white px-4 py-2 rounded-3xl hover:bg-[#5a2e9b]">
-                Signup
-              </Button>
+              <Link to="/login">
+                <Button
+                  className="border  border-gray-300 text-gray-700 px-4 py-2 rounded-3xl hover:border-gray-500"
+                  variant="outline"
+                >
+                  Login
+                </Button>
+              </Link>
+              <Link to="/signup">
+                <Button className="bg-[#6A38C2] text-white px-4 py-2 rounded-3xl hover:bg-[#5a2e9b]">
+                  Signup
+                </Button>
+              </Link>
             </div>
           ) : (
             <Popover>
