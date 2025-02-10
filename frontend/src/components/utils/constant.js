87aa59aaef4704108b755f1e1a74d0ae19fb38b1
Rishav <rishavkamboj75@gmail.com>
2025-02-11 00:36:48 +0530

    New commit

--- frontend/src/components/utils/constant.js
+++ frontend/src/components/utils/constant.js
@@ -1,8 +1,4 @@
-export const USER_API = "http://localhost:3001/api/v1/user/";
-
-export const JOB_API = "http://localhost:3001/api/v1/jobs/";
-
-export const APPLICATION_API = "http://localhost:3001/api/v1/application/";
-
-export const COMPANY_API = "http://localhost:3001/api/v1/company/";
-
+export const USER_API = "https://apnijob.onrender.com/api/v1/user/";
+export const JOB_API = "https://apnijob.onrender.com/api/v1/jobs/";
+export const APPLICATION_API ="https://apnijob.onrender.com/api/v1/application/";
+export const COMPANY_API = "https://apnijob.onrender.com/api/v1/company/";
