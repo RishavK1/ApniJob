2c03843f72cd18f2b7a080a6cd14fc4c03a04d50
Rishav <rishavkamboj75@gmail.com>
2025-02-12 14:59:54 +0530

    New commit

--- frontend/src/hooks/useGetAllAppliedJobs.jsx
+++ frontend/src/hooks/useGetAllAppliedJobs.jsx
@@ -7,41 +7,41 @@ import { useDispatch } from "react-redux";
 const useGetAllAppliedJobs = () => {
   const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   const fetchAppliedJobs = async () => {
-  //     try {
-  //         const res = await axios.get(`${APPLICATION_API}/get`, {
-  //             withCredentials: true,
-  //         });
-  //         if (res.data.success) {
-  //             dispatch(setAllAppledJobs(res.data.application));
-  //         }
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //     };
-  //     fetchAppliedJobs();
-  // },[]);
-useEffect(() => {
-  const fetchAppliedJobs = async () => {
-    try {
-      const res = await axios.get(`${APPLICATION_API}/get`, {
-        withCredentials: true,
-      });
-      console.log("API Response:", res.data); // Debugging
-
-      if (res.data.success && res.data.applications) {
-        dispatch(setAllAppliedJobs(res.data.applications));
-      } else {
-        dispatch(setAllAppliedJobs([])); // Set an empty array to prevent undefined issues
+  useEffect(() => {
+    const fetchAppliedJobs = async () => {
+      try {
+          const res = await axios.get(`${APPLICATION_API}/get`, {
+              withCredentials: true,
+          });
+          if (res.data.success) {
+              dispatch(setAllAppliedJobs(res.data.application));
+          }
+      } catch (error) {
+        console.log(error);
       }
-    } catch (error) {
-      console.error("Error fetching applied jobs:", error);
-      dispatch(setAllAppliedJobs([])); // Prevent crashes by setting an empty array
-    }
-  };
-  fetchAppliedJobs();
-}, [dispatch]);
+      };
+      fetchAppliedJobs();
+  },[]);
+// useEffect(() => {
+//   const fetchAppliedJobs = async () => {
+//     try {
+//       const res = await axios.get(`${APPLICATION_API}/get`, {
+//         withCredentials: true,
+//       });
+//       console.log("API Response:", res.data); // Debugging
+
+//       if (res.data.success && res.data.applications) {
+//         dispatch(setAllAppliedJobs(res.data.applications));
+//       } else {
+//         dispatch(setAllAppliedJobs([])); // Set an empty array to prevent undefined issues
+//       }
+//     } catch (error) {
+//       console.error("Error fetching applied jobs:", error);
+//       dispatch(setAllAppliedJobs([])); // Prevent crashes by setting an empty array
+//     }
+//   };
+//   fetchAppliedJobs();
+// }, [dispatch]);
 
 };
 
